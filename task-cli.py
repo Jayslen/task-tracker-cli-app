8e7ddbe68b901820b6885e9e8fd380408d31dca1
jayslen <jayslen134@gmail.com>
2024-11-01 23:48:40 -0400

    add: task creation, update id, styles table

--- task-cli.py
+++ task-cli.py
@@ -4,6 +4,7 @@ import os
 from datetime import datetime
 from rich.console import Console
 from rich.table import Table
+from rich import box
 
 
 class MyCLI(cmd.Cmd):
@@ -17,6 +18,7 @@ class MyCLI(cmd.Cmd):
 
     def update_tasks(self, value):
         self.tasks = value
+        self.id = value[-1]["id"] + 1
 
     def create_task(self, name):
         return {
@@ -52,7 +54,24 @@ class MyCLI(cmd.Cmd):
         new_task = self.create_task(line)
         self.tasks.append(new_task)
         self.id = self.tasks[len(self.tasks) - 1]["id"] + 1
-        print(self.id)
+
+    def do_show_tasks(self, line):
+        rows = [list(x.values()) for x in self.tasks]
+        columns = list(self.tasks[0].keys())
+
+        table = Table(title="Tasks", show_lines=True, box=box.DOUBLE_EDGE)
+        console = Console()
+
+        for x in columns:
+            table.add_column(x)
+
+        for x in rows:
+            row = list(x)
+            row[0] = str(row[0])
+            row[1] = row[1].capitalize()
+            table.add_row(*row, style="magenta")
+
+        console.print(table)
 
 
 if __name__ == "__main__":
